9b72ecb335c6b7d05ea48404acc866a8cda3db72
DogukanTorun1 <ddogukan97@gmail.com>
2024-06-13 02:14:02 +0300

    controller added

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
 const app = express();
 
-mongoose.connect("mongodb+srv://ddogukan97:KizARDgPbNezx199@cluster0.ptquegs.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-  console.log('Connected to database!');
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
-}).catch(() => {
-    console.log('Connection failed!');
-    });
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// routes
+app.use("/api/products", productRoute);
 
 
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
 });
+
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://ddogukan97:KizARDgPbNezx199@cluster0.ptquegs.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+
+
+
+
--- models/product.model.js
+++ models/product.model.js
@@ -1,15 +1,35 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema(
+const ProductSchema = mongoose.Schema(
   {
-    _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: [true, "Please enter name"] },
-    quantity: { type: Number, required: true, default: 0 },
-    image: { type: String, required: false },
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    image: {
+      type: String,
+      required: false,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-const Product = mongoose.model("Product", productSchema);
+
+const Product = mongoose.model("Product", ProductSchema);
 
 module.exports = Product;
